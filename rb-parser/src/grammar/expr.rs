d64446384be0b703bb73ba6d75f4c706abc8f7ad
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-17 19:33:09 -0700

    Can build up call exprs

--- rb-parser/src/grammar/expr.rs
+++ rb-parser/src/grammar/expr.rs
@@ -2,13 +2,14 @@ use crate::{CompletedMarker, Marker, T};
 
 use super::*;
 
-// test ok
-// print("hello")
-pub fn expr(p: &mut Parser) {
-  let m = p.start();
-  let Some(lhs) = atom_expr(p, m) else { return };
+pub fn expr(p: &mut Parser) { let _ = expr0(p); }
 
-  lhs.pos;
+fn expr0(p: &mut Parser) -> Option<CompletedMarker> {
+  let m = p.start();
+  let lhs = atom_expr(p, m)?;
+  let _m = postfix_expr(p, lhs);
+
+  None
 }
 
 fn atom_expr(p: &mut Parser, m: Marker) -> Option<CompletedMarker> {
@@ -26,4 +27,43 @@ fn atom_expr(p: &mut Parser, m: Marker) -> Option<CompletedMarker> {
       None
     }
   }
+}
+
+fn postfix_expr(p: &mut Parser, mut lhs: CompletedMarker) -> CompletedMarker {
+  loop {
+    lhs = match p.current() {
+      // test ok
+      // hi(3)
+      T!['('] => {
+        let call = lhs.precede(p);
+
+        let m = p.start();
+        paren_args(p);
+        m.complete(p, PAREN_ARGS);
+
+        call.complete(p, CALL_EXPR)
+      }
+
+      _ => break,
+    };
+  }
+  lhs
+}
+
+fn paren_args(p: &mut Parser) {
+  p.eat(T!['(']);
+
+  while !p.at(EOF) && !p.at(T![')']) {
+    expr(p);
+    if p.at(T![,]) {
+      p.bump();
+      if p.at(T![')']) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  p.expect(T![')']);
 }
