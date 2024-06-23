5d8a78b1868758cb1d6b35f8051eeae63d443bc5
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-23 14:50:53 -0700

    Add command line options to refmt to accept input from stdin

--- refmt/src/main.rs
+++ refmt/src/main.rs
@@ -1,30 +1,53 @@
 use rb_diagnostic::{emit, Source, Sources, Span};
 use rb_syntax::cst;
-use std::{path::PathBuf, sync::Arc};
+use std::{io::Read, path::PathBuf, sync::Arc};
 
 use clap::Parser;
 
 #[derive(Parser)]
 struct Args {
-  input: PathBuf,
+  #[clap(short, long)]
+  input: Option<PathBuf>,
+
+  #[clap(long)]
+  stdin: bool,
 }
 
 fn main() {
   let args = Args::parse();
 
-  let source = match std::fs::read_to_string(&args.input) {
-    Ok(s) => s,
-    Err(e) => {
-      eprintln!("Failed to read file {}: {}", args.input.display(), e);
+  let source = if let Some(ref input) = args.input {
+    if args.stdin {
+      eprintln!("Cannot specify both --input and --stdin");
       std::process::exit(1);
     }
+
+    match std::fs::read_to_string(input) {
+      Ok(s) => s,
+      Err(e) => {
+        eprintln!("Failed to read file {}: {}", input.display(), e);
+        std::process::exit(1);
+      }
+    }
+  } else if args.stdin {
+    if args.input.is_some() {
+      eprintln!("Cannot specify both --input and --stdin");
+      std::process::exit(1);
+    }
+
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    s
+  } else {
+    eprintln!("Must specify either --input or --stdin");
+    std::process::exit(1);
   };
 
-  let name = args.input.to_string_lossy().into_owned();
+  let name = "input.rbr";
   let cst = cst::SourceFile::parse(&source);
 
   let mut sources = Sources::new();
-  let file = sources.add(Source::new(name, source));
+  let file = sources.add(Source::new(name.into(), source));
 
   rb_diagnostic::run_or_exit(Arc::new(sources), || {
     for error in cst.errors() {
