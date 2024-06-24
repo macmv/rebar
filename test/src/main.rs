86c38dd6594117750f2c7736ce39e8d082308c79
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-23 19:44:42 -0700

    Start working on integration tests

---
+++ test/src/main.rs
@@ -0,0 +1,32 @@
+use std::path::{Path, PathBuf};
+
+fn main() {
+  let files = gather_files(Path::new("test/integration"));
+
+  for path in files {
+    if path.extension().unwrap() == "rbr" {
+      let name = path.file_stem().unwrap().to_str().unwrap();
+      println!("Running test: {}", name);
+    }
+  }
+}
+
+fn gather_files(dir: &Path) -> Vec<PathBuf> {
+  let mut files = Vec::new();
+
+  gather_files_inner(dir, &mut files);
+
+  files
+}
+
+fn gather_files_inner(dir: &Path, files: &mut Vec<PathBuf>) {
+  for entry in std::fs::read_dir(dir).unwrap() {
+    let entry = entry.unwrap();
+    let path = entry.path();
+    if path.is_dir() {
+      files.extend(gather_files(&path));
+    } else {
+      files.push(path);
+    }
+  }
+}
