f44406f2f368ed54f3c88acf97fb3778e12e5aa8
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-26 22:28:40 -0700

    Remove println in assert_eq

--- rb-runtime/src/stdlib/core/mod.rs
+++ rb-runtime/src/stdlib/core/mod.rs
@@ -5,7 +5,6 @@ impl Environment {
     let mut env = Environment::empty();
 
     env.add_fn("assert_eq", |a: i64, b: i64| {
-      println!("assert_eq({}, {})", a, b);
       if a != b {
         panic!("assertion failed: `(left == right)`\n  left: `{}`,\n right: `{}`", a, b);
       }
