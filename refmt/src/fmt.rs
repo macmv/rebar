0c27a4c6dd61a790b2bb9eea06c3a59f96235fa1
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-26 22:56:50 -0700

    Update newline handling to work with leading whitespace instead of trailing whitespace

--- refmt/src/fmt.rs
+++ refmt/src/fmt.rs
@@ -16,55 +16,70 @@ fn is_whitespace(kind: SyntaxKind) -> bool { matches!(kind, T![ws] | T![nl]) }
 
 impl Formatter {
   fn fmt_leading_whitespace(&self, node: &SyntaxNode) -> String {
-    // Iterate backwards until we find the first non-whitespace node.
-    let mut curr = NodeOrToken::from(node.clone());
+    let mut out = String::new();
     let mut found_ws = false;
-    while let Some(ws) = curr.prev_sibling_or_token() {
-      if !is_whitespace(ws.kind()) {
+    for child in node.children_with_tokens() {
+      if !is_whitespace(child.kind()) {
         break;
       }
-      curr = ws;
       found_ws = true;
-    }
-    if !found_ws {
-      return "".into();
-    }
 
-    let is_start = curr.prev_sibling_or_token().is_none();
-
-    // Iterate forward and format said whitespace.
-    let mut out = String::new();
-    loop {
-      let s = match curr {
+      let s = match child {
         NodeOrToken::Node(ref n) => n.text().to_string(),
         NodeOrToken::Token(ref n) => n.text().to_string(),
       };
 
       out += &s;
-
-      if let Some(node) = curr.next_sibling_or_token() {
-        curr = node;
-        if !is_whitespace(curr.kind()) {
-          break;
-        }
-      } else {
-        break;
-      }
     }
 
-    let out = out.trim_end_matches(' ');
-    let out = out.strip_suffix('\n').unwrap_or(out);
+    if !found_ws {
+      return "".into();
+    }
+
+    // Act line there's a newline before the file.
+    self.fmt_whitespace(format!("\n{out}")).trim_start().to_string()
+  }
+
+  fn fmt_trailing_whitespace(&self, node: &SyntaxNode) -> String {
+    let mut out = String::new();
+    let mut curr = NodeOrToken::from(node.clone());
+    let mut found_ws = false;
+    while let Some(ws) = curr.next_sibling_or_token() {
+      found_ws = true;
+      if !is_whitespace(ws.kind()) {
+        break;
+      }
+
+      let s = match ws {
+        NodeOrToken::Node(ref n) => n.text().to_string(),
+        NodeOrToken::Token(ref n) => n.text().to_string(),
+      };
+
+      out += &s;
+      curr = ws;
+    }
+
+    if !found_ws {
+      return "".into();
+    }
+
+    self.fmt_whitespace(out)
+  }
+
+  fn fmt_whitespace(&self, whitespace: String) -> String {
+    let out = whitespace.trim_start_matches(' ').trim_end_matches(' ');
+    // let out = out.strip_suffix('\n').unwrap_or(out);
 
     // `res` is the resulting lines between the previous statement and `node`.
     let mut res: Vec<&str> = vec![];
 
-    const EMPTY_LINE_THRESHOLD: usize = 2;
+    const EMPTY_LINE_THRESHOLD: usize = 3;
 
     let mut empty_lines = 0;
 
-    let end: &[&str] = if out.ends_with('\n') { &[""] } else { &[] };
-
-    for line in out.lines().chain(end.into_iter().copied()) {
+    // Note that .lines() trims the last line (but only sometimes), and .split('\n')
+    // does not.
+    for line in out.split('\n').skip(1) {
       let line = line.trim();
 
       if line.is_empty() {
@@ -80,29 +95,29 @@ impl Formatter {
       res.push(line);
     }
 
+    // Don't keep the last line.
+    res.pop();
+
     let mut out = String::new();
 
-    // Add the newline from the previous statement.
-    if !is_start {
-      out += "\n";
-    }
-
+    out += "\n";
     for line in res {
       out += line;
       out += "\n";
     }
+
     out
   }
 
   pub fn fmt_stmt(&mut self, stmt: &cst::Stmt) -> String {
-    let prefix = self.fmt_leading_whitespace(stmt.syntax());
-
     let s = match stmt {
       cst::Stmt::ExprStmt(expr) => self.fmt_expr(&expr.expr().unwrap()),
       _ => todo!("stmt {stmt:?}"),
     };
 
-    format!("{prefix}{s}")
+    let suffix = self.fmt_trailing_whitespace(stmt.syntax());
+
+    format!("{s}{suffix}")
   }
 
   pub fn fmt_expr(&mut self, expr: &cst::Expr) -> String {
@@ -157,10 +172,10 @@ pub fn format(cst: &cst::SourceFile) -> String {
   let mut out = String::new();
   let mut fmt = Formatter::default();
 
+  out += &fmt.fmt_leading_whitespace(cst.syntax());
   for stmt in cst.stmts() {
     out += &fmt.fmt_stmt(&stmt);
   }
-  out += "\n";
 
   out
 }
@@ -170,10 +185,10 @@ pub fn format(cst: &cst::SourceFile) -> String {
 pub fn format_opts(cst: &cst::SourceFile, mut fmt: Formatter) -> String {
   let mut out = String::new();
 
+  out += &fmt.fmt_leading_whitespace(cst.syntax());
   for stmt in cst.stmts() {
     out += &fmt.fmt_stmt(&stmt);
   }
-  out += "\n";
 
   out
 }
