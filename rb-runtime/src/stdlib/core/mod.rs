1e252fd270faea0bab9847a2a3a354afa60a53aa
Neil Macneale V <neil.macneale.v@gmail.com>
2024-06-26 22:24:26 -0700

    Start implementing assert_eq correctly

--- rb-runtime/src/stdlib/core/mod.rs
+++ rb-runtime/src/stdlib/core/mod.rs
@@ -4,7 +4,12 @@ impl Environment {
   pub fn core() -> Self {
     let mut env = Environment::empty();
 
-    env.add_fn("assert_eq", |a: i64, b: i64| -> i64 { (a == b) as i64 });
+    env.add_fn("assert_eq", |a: i64, b: i64| {
+      println!("assert_eq({}, {})", a, b);
+      if a != b {
+        panic!("assertion failed: `(left == right)`\n  left: `{}`,\n right: `{}`", a, b);
+      }
+    });
 
     env
   }
