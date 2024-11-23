17a6737f2f96dbb4a224bb8662f06c433633158e
Neil Macneale V <neil.macneale.v@gmail.com>
2024-11-22 21:52:20 -0800

    Remove unused import

--- rb-jit/src/jit.rs
+++ rb-jit/src/jit.rs
@@ -11,10 +11,7 @@ use rb_mir::ast::{self as mir, UserFunctionId};
 use rb_typer::{Literal, Type};
 use std::{collections::HashMap, marker::PhantomPinned, mem::ManuallyDrop};
 
-use crate::{
-  array::RbArray,
-  value::{CompactValues, DynamicValueType, ParamKind, RValue, Value, ValueType},
-};
+use crate::value::{CompactValues, DynamicValueType, ParamKind, RValue, Value, ValueType};
 
 pub struct JIT {
   module: JITModule,
