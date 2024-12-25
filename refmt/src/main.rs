de1efdc4da04d192ab9c712eb7043ce2c7c78d17
Neil Macneale V <neil.macneale.v@gmail.com>
2024-12-24 22:49:11 -0800

    Update refmt/main.rs to format entire directories

--- refmt/src/main.rs
+++ refmt/src/main.rs
@@ -1,7 +1,7 @@
 use rb_diagnostic::{emit, Source, Sources, Span};
 use rb_syntax::cst;
 use std::{
-  io::Read,
+  io::{self, Read},
   path::{Path, PathBuf},
   sync::Arc,
 };
@@ -22,20 +22,60 @@ struct Args {
 fn main() {
   let args = Args::parse();
 
-  let source = if args.input == Path::new("-") {
-    let mut s = String::new();
-    std::io::stdin().read_to_string(&mut s).unwrap();
-    s
+  let res = if args.input == Path::new("-") {
+    format_stdin()
   } else {
-    match std::fs::read_to_string(&args.input) {
-      Ok(s) => s,
-      Err(e) => {
-        eprintln!("Failed to read file {}: {}", args.input.display(), e);
-        std::process::exit(1);
-      }
+    if args.input.is_dir() {
+      format_dir(&args.input)
+    } else {
+      format_file(&args.input)
     }
   };
 
+  match res {
+    Ok(_) => {}
+    Err(err) => {
+      eprintln!("{}", err);
+      std::process::exit(1);
+    }
+  }
+}
+
+fn format_stdin() -> Result<(), io::Error> {
+  let mut source = String::new();
+  io::stdin().read_to_string(&mut source)?;
+
+  let formatted = format_str(source)?;
+
+  print!("{}", formatted);
+
+  Ok(())
+}
+
+fn format_dir(dir: &Path) -> Result<(), io::Error> {
+  for entry in std::fs::read_dir(dir)? {
+    let entry = entry?;
+    let path = entry.path();
+
+    if path.is_dir() {
+      format_dir(&path)?;
+    } else if path.extension().is_some_and(|ext| ext == "rbr") {
+      format_file(&path)?;
+    }
+  }
+
+  Ok(())
+}
+
+fn format_file(path: &Path) -> Result<(), io::Error> {
+  let source = std::fs::read_to_string(&path)?;
+
+  let formatted = format_str(source)?;
+
+  std::fs::write(path, formatted)
+}
+
+fn format_str(source: String) -> Result<String, io::Error> {
   let name = "input.rbr";
   let cst = cst::SourceFile::parse(&source);
 
@@ -48,6 +88,5 @@ fn main() {
     }
   });
 
-  let out = fmt::format(&cst.tree());
-  print!("{out}");
+  Ok(fmt::format(&cst.tree()))
 }
