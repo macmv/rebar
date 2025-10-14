9ad92d08b0e23111c3ca9d53daa1ef5d3bf743d6
Neil Macneale V <neil.macneale.v@gmail.com>
2025-10-14 00:37:25 -0700

    Sort opcodes

--- rb-codegen-x86/src/instruction.rs
+++ rb-codegen-x86/src/instruction.rs
@@ -187,10 +187,10 @@ impl Instruction {
 }
 
 impl Opcode {
-  pub const MOV_RM_IMM_8: Opcode = Opcode::new([0xc6]);
-  pub const MOV_RM_IMM_16: Opcode = Opcode::new([0xc7]);
-  pub const LEA: Opcode = Opcode::new([0x8d]);
   pub const INT3: Opcode = Opcode::new([0xcc]);
-  pub const SYSCALL: Opcode = Opcode::new([0x0f, 0x05]);
+  pub const LEA: Opcode = Opcode::new([0x8d]);
+  pub const MOV_RM_IMM_16: Opcode = Opcode::new([0xc7]);
+  pub const MOV_RM_IMM_8: Opcode = Opcode::new([0xc6]);
   pub const RET: Opcode = Opcode::new([0xc3]);
+  pub const SYSCALL: Opcode = Opcode::new([0x0f, 0x05]);
 }
