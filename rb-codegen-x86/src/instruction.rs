684f5c1759228678731b06e13433d2dad1dfb153
Neil Macneale V <neil.macneale.v@gmail.com>
2025-10-24 11:26:38 -0700

    Add the other 8 register indicies

--- rb-codegen-x86/src/instruction.rs
+++ rb-codegen-x86/src/instruction.rs
@@ -30,6 +30,15 @@ pub enum RegisterIndex {
   Ebp,
   Esi,
   Edi,
+
+  R8,
+  R9,
+  R10,
+  R11,
+  R12,
+  R13,
+  R14,
+  R15,
 }
 
 #[derive(Clone, Copy)]
@@ -213,7 +222,7 @@ impl Instruction {
 impl RegisterIndex {
   #[track_caller]
   pub fn from_usize(value: usize) -> Self {
-    if value >= 8 {
+    if value >= 16 {
       panic!("invalid register index: {}", value);
     }
 
--- rb-codegen-x86/src/register.rs
+++ rb-codegen-x86/src/register.rs
@@ -50,6 +50,14 @@ registers! {
   BPL: "bpl", Bit8, Ebp;
   SIL: "sil", Bit8, Esi;
   DIL: "dil", Bit8, Edi;
+  R8B: "r8b", Bit8, R8;
+  R9B: "r9b", Bit8, R9;
+  R10B: "r10b", Bit8, R10;
+  R11B: "r11b", Bit8, R11;
+  R12B: "r12b", Bit8, R12;
+  R13B: "r13b", Bit8, R13;
+  R14B: "r14b", Bit8, R14;
+  R15B: "r15b", Bit8, R15;
 
   AX: "ax", Bit16, Eax;
   CX: "cx", Bit16, Ecx;
@@ -59,6 +67,14 @@ registers! {
   BP: "bp", Bit16, Ebp;
   SI: "si", Bit16, Esi;
   DI: "di", Bit16, Edi;
+  R8W: "r8w", Bit16, R8;
+  R9W: "r9w", Bit16, R9;
+  R10W: "r10w", Bit16, R10;
+  R11W: "r11w", Bit16, R11;
+  R12W: "r12w", Bit16, R12;
+  R13W: "r13w", Bit16, R13;
+  R14W: "r14w", Bit16, R14;
+  R15W: "r15w", Bit16, R15;
 
   EAX: "eax", Bit32, Eax;
   ECX: "ecx", Bit32, Ecx;
@@ -68,6 +84,14 @@ registers! {
   EBP: "ebp", Bit32, Ebp;
   ESI: "esi", Bit32, Esi;
   EDI: "edi", Bit32, Edi;
+  R8D: "r8d", Bit32, R8;
+  R9D: "r9d", Bit32, R9;
+  R10D: "r10d", Bit32, R10;
+  R11D: "r11d", Bit32, R11;
+  R12D: "r12d", Bit32, R12;
+  R13D: "r13d", Bit32, R13;
+  R14D: "r14d", Bit32, R14;
+  R15D: "r15d", Bit32, R15;
 
   RAX: "rax", Bit64, Eax;
   RCX: "rcx", Bit64, Ecx;
@@ -77,4 +101,12 @@ registers! {
   RBP: "rbp", Bit64, Ebp;
   RSI: "rsi", Bit64, Esi;
   RDI: "rdi", Bit64, Edi;
+  R8: "r8", Bit64, R8;
+  R9: "r9", Bit64, R9;
+  R10: "r10", Bit64, R10;
+  R11: "r11", Bit64, R11;
+  R12: "r12", Bit64, R12;
+  R13: "r13", Bit64, R13;
+  R14: "r14", Bit64, R14;
+  R15: "r15", Bit64, R15;
 }
