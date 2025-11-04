b263040fb3059277bd65ba8577473e1af3d73bf0
Neil Macneale V <neil.macneale.v@gmail.com>
2025-11-03 21:31:45 -0800

    Filter tests before making threads

--- test/src/main.rs
+++ test/src/main.rs
@@ -8,7 +8,8 @@ use std::{
 fn main() -> ExitCode {
   let filter = std::env::args().nth(1).unwrap_or_default();
 
-  let files = gather_files(Path::new("test/integration"));
+  let mut files = gather_files(Path::new("test/integration"));
+  files.retain(|path| path.display().to_string().contains(&filter));
 
   println!("running tests...");
 
@@ -18,42 +19,42 @@ fn main() -> ExitCode {
   let failed = std::sync::atomic::AtomicBool::new(false);
   let std = Path::new("lib/std/lib.rbr");
 
+  let out_dir = Path::new("out");
+  std::fs::remove_dir_all(&out_dir).unwrap();
+
   std::thread::scope(|scope| {
     for (index, chunk) in files.chunks(chunk_size).enumerate() {
-      let f = &filter;
       let failed = &failed;
       scope.spawn(move || {
-        let out_dir = Path::new("out").join(format!("thread-{index}"));
+        let out_dir = out_dir.join(format!("thread-{index}"));
         std::fs::create_dir_all(&out_dir).unwrap();
         let out = out_dir.join("out.o");
 
         for path in chunk {
           let stringified = path.display().to_string();
-          if stringified.contains(f) {
-            let res = catch_unwind(|| match rb_runtime::compile_test(path, std, &out) {
-              (_, Ok(_)) => println!("{}... \x1b[32mok\x1b[0m", stringified),
-              (sources, Err(diagnostics)) => {
-                failed.fetch_or(true, Ordering::AcqRel);
-                println!("{}... \x1b[31mfail\x1b[0m", stringified);
-                for d in diagnostics {
-                  println!("{}", d.render_with_color(&sources));
-                }
+          let res = catch_unwind(|| match rb_runtime::compile_test(path, std, &out) {
+            (_, Ok(_)) => println!("{}... \x1b[32mok\x1b[0m", stringified),
+            (sources, Err(diagnostics)) => {
+              failed.fetch_or(true, Ordering::AcqRel);
+              println!("{}... \x1b[31mfail\x1b[0m", stringified);
+              for d in diagnostics {
+                println!("{}", d.render_with_color(&sources));
               }
-            });
+            }
+          });
 
-            match res {
-              Ok(_) => {}
-              Err(e) => {
-                failed.fetch_or(true, Ordering::AcqRel);
-                println!("{}... \x1b[31mpanic\x1b[0m", stringified);
+          match res {
+            Ok(_) => {}
+            Err(e) => {
+              failed.fetch_or(true, Ordering::AcqRel);
+              println!("{}... \x1b[31mpanic\x1b[0m", stringified);
 
-                if let Some(s) = e.downcast_ref::<String>() {
-                  println!("{}", s);
-                } else if let Some(s) = e.downcast_ref::<&str>() {
-                  println!("{}", s);
-                } else {
-                  println!("{:?}", e);
-                }
+              if let Some(s) = e.downcast_ref::<String>() {
+                println!("{}", s);
+              } else if let Some(s) = e.downcast_ref::<&str>() {
+                println!("{}", s);
+              } else {
+                println!("{:?}", e);
               }
             }
           }
